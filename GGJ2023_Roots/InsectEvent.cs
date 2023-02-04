afeedef227b6b201156b0a5386285bf5df7347fe
barbosayyy <72970256+barbosayyy@users.noreply.github.com>
2023-02-04 02:50:44 +0000

    Game events base class, insect event class and game manager

---
+++ GGJ2023_Roots/InsectEvent.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InsectEvent : Event
+{
+    private float elapsedTime;
+
+    private void Start()
+    {
+        OnStartEvent();
+    }
+
+    private void Update()
+    {
+        elapsedTime -= Time.deltaTime;
+
+        if (elapsedTime < this.lifetime)
+        {
+            OnEndEvent();
+        }
+    }
+
+    public override void OnStartEvent()
+    {
+        base.OnStartEvent();
+    }
+
+    public override void OnEndEvent()
+    {
+        // Behaviour code here before destroy
+
+        base.OnEndEvent();
+    }
+}
